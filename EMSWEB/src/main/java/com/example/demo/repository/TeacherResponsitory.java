384f57bd765260794a87270c69513a2871f7888b
nvtuanlv <nvtuanlv@gmail.com>
2020-11-16 17:51:41 +0700

    commit

--- EMSWEB/src/main/java/com/example/demo/repository/TeacherResponsitory.java
+++ EMSWEB/src/main/java/com/example/demo/repository/TeacherResponsitory.java
@@ -3,9 +3,10 @@ package com.example.demo.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Teacher;
-
+@Repository("teacherResponsitory")
 public interface TeacherResponsitory extends JpaRepository<Teacher, String>{
-	List<Teacher> findByFname(String fname);
+//	List<Teacher> findByFname(String fname);
 }
