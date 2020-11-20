580530a9ddf536d2d7c2467cbb12e8f879737bdf
tritran <Tritvps09334@fpt.edu.vn>
2020-11-20 18:02:38 +0700

    a

--- EMSWEB/src/main/java/com/example/demo/repository/StudentResponsitory.java
+++ EMSWEB/src/main/java/com/example/demo/repository/StudentResponsitory.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Student;
 @Repository("studentResponsitory")
